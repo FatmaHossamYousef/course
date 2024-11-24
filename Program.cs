9427ace537ba0fe607c067ff97c734184ff98b14
FatmaHossamYousef <131831009+FatmaHossamYousef@users.noreply.github.com>
2024-11-25 00:35:59 +0200

    Program.cs

--- Program.cs
+++ Program.cs
@@ -3,183 +3,211 @@ using System;
 using System.ComponentModel.Design;
 using System.Diagnostics;
 using static System.Net.Mime.MediaTypeNames;
-
-namespace task1
+namespace task3
 {
+    class Book
+    {
+        string title;
+        string author;
+        string ISBN;
+        bool availability;
+
+        public Book(string title = " ", string author = " ", string ISBN = " ", bool availability = true)
+        {
+            this.title = title;
+            this.author = author;
+            this.ISBN = ISBN;
+            this.availability = availability;
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+        public string GetAuthor()
+        {
+            return author;
+        }
+        public string GetISBN()
+        {
+            return ISBN;
+        }
+        public bool GetVailability()
+        {
+            return availability;
+        }
+
+
+
+        public void SetTitle(string title)
+        {
+            this.title = title;
+        }
+        public void SetAuthor(string author)
+        {
+            this.author = author;
+        }
+        public void SetISBN(string ISBN)
+        {
+            this.ISBN = ISBN;
+        }
+        public void SetVailability(bool availability)
+        {
+            this.availability = availability;
+        }
+    }
+
+
+
+
+    class Library
+    {
+        public List<Book> books;
+
+        public Library()
+        {
+            this.books = new List<Book>();
+        }
+
+        // add book to library
+        
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+        //طريقه تانيه 
+        //public void AddBook (string title = " ", string author = " ", string ISBN = "", bool availability = true)
+
+        //{
+        //    Book book = new Book();
+        //    book.SetTitle(title);
+        //    book.SetAuthor(author);
+        //    book.SetISBN(ISBN);
+        //    book.SetVailability(availability);
+        //    books.Add(book);
+        //  }
+
+        // search for book in library by title or author name
+
+        public void SearchBook(string search)
+        {
+            bool found = false;
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (books[i].GetTitle().Contains(search) || books[i].GetAuthor().Contains(search))
+                {// i use contains if user enter any part  of book name or outher name  program will return all possible answers
+
+                    found = true;
+                    Console.WriteLine();
+                    Console.WriteLine(books[i].GetTitle());
+                    Console.WriteLine(books[i].GetAuthor());
+                    Console.WriteLine(books[i].GetISBN());
+                    Console.WriteLine(books[i].GetVailability());
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine("================");
+
+            if (!found)
+            {
+                Console.WriteLine("\nbook not in library");
+               
+            }
+        }
+        //borrow book 
+        public void BorrowBook(string search)
+        {
+            bool borrow = false;
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (books[i].GetTitle().Contains(search) &&  books[i].GetVailability()==true)
+                {
+
+                    books[i].SetVailability(false);
+                    borrow = true;
+                }
+            }
+
+            if (!borrow)
+            {
+                Console.WriteLine(" book is not in the library.");
+            }
+        }
+        //return book
+        public void ReturnBook(string search)
+        {
+            bool exist = false;
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (books[i].GetTitle().Contains(search) && books[i].GetVailability() == false)
+                {
+                    books[i].SetVailability(true);
+                    exist = true;
+                }
+            }
+            if (!exist)
+            {
+                Console.WriteLine(" book is not borrowed");
+            }
+        }
+    }
+
+
+    
     internal class Program
     {
         static void Main(string[] args)
         {
+            Library library = new Library();
 
-            List<int> myList = new List<int>();
-            char input = ' ';
-            Console.WriteLine("   =============menu options========");
-            Console.WriteLine("       p - print numbers");
-            Console.WriteLine("       A - Add a number");
-            Console.WriteLine("       M - Display mean of the numbers ");
-            Console.WriteLine("       S - Display the smallest number");
-            Console.WriteLine("       L - Display the largest number");
-            Console.WriteLine("       F - Find anumber");
-            Console.WriteLine("       C - Clear the whole list");
-            Console.WriteLine("       Q - Quit");
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("enter your choice from menu");
+            //Adding books to the library
+            library.AddBook(new Book("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565"));
+            library.AddBook(new Book("To Kill a Mockingbird", "Harper Lee", "9780061120084"));
+            library.AddBook(new Book("1984", "George Orwell", "9780451524935"));
+            library.AddBook(new Book("ozil", "George Orwell", "9780451545935"));
 
 
-            do
-            {
-                Console.Write("==> ");
-                input = char.Parse(Console.ReadLine());
+            //طريقه استخدام الطريقه التانيه
+            //library.AddBook("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565");
+            //library.AddBook("To Kill a Mockingbird", "Harper Lee", "9780061120084");
+            //library.AddBook("1984", "George Orwell", "9780451524935");
 
 
-                if (input == 'p' || input == 'P')
-                {
-                    if (myList.Count == 0)
-                    {
-                        Console.WriteLine("[] - The list is empty ");
-                    }
-                    else
-                    {
-                        Console.Write("[ ");
-                        for (int i = 0; i < myList.Count; i++)
-                        {
-                            Console.Write(myList[i] + " ");
+            // Searching  books
+            Console.WriteLine("searching result :-");
+            library.SearchBook("Orwell");
 
-                        }
-                        Console.WriteLine("] ");
-                    }
-                }
+            // borrowing  books
+            Console.WriteLine("borrowing  books...");
+            library.BorrowBook("Gatsby");
+            library.BorrowBook("1984");
+            library.BorrowBook("Harry Potter"); // This book is not in the library
+            Console.WriteLine("\n================");
 
 
-                else if (input == 'a' || input == 'A')
-                {
-                    Console.Write("Enter the number to add : ==> ");
-                    int newItem = int.Parse(Console.ReadLine());
-
-                    // ===========for ponus  do not allow duplicate ==============
-                    bool exist = false;
-                    for (int i = 0; i < myList.Count; i++)
-                    {
-                        if (myList[i] == newItem)
-                        {
-                            exist = true;
-                            Console.WriteLine("this number is exist input another number");
-                            break;
-                        }
-                    }
-                    if (!exist)
-                    {
-                        myList.Add(newItem);
-                        Console.WriteLine($"{newItem} added");
-                    }
-                }
-
-
-                else if (input == 'm' || input == 'M')
-                {
-                    if (myList.Count == 0)
-                    {
-                        Console.WriteLine("Unable to calculate the mean - no data");
-                    }
-
-                    double sum = 0.0;
-                    double mean = 0.0;
-                    for (int i = 0; i < myList.Count; i++)
-                    {
-                        sum += myList[i];
-                    }
-                    mean = sum / myList.Count;
-                    Console.WriteLine($"Mean: {mean}");
-                }
-
-
-                else if (input == 's' || input == 'S')
-                {
-                    if (myList.Count == 0)
-                    {
-                        Console.WriteLine("Unable to detemine the smallest number -list is empty");
-                    }
-                    int smallest = myList[0];
-                    for (int i = 0; i < myList.Count; i++)
-                    {
-                        if (myList[i] < smallest)
-                        {
-                            smallest = myList[i];
-                        }
-                    }
-                    Console.WriteLine($"the smallest uumber is {smallest}");
-                }
-
-
-                else if (input == 'l' || input == 'L')
-                {
-                    if (myList.Count == 0)
-                    {
-                        Console.WriteLine("Unable to detemine the largest number -list is empty");
-                    }
-                    int largest = myList[0];
-                    for (int i = 0; i < myList.Count; i++)
-                    {
-                        if (myList[i] > largest)
-                        {
-                            largest = myList[i];
-                        }
-                    }
-                    Console.WriteLine($"the largest uumber is {largest}");
-                }
-
-                //==========search task===========
-                else if (input == 'f' || input == 'F')
-                {
-                    Console.Write("Enter number to search fo it : ");
-                    int n = int.Parse(Console.ReadLine());
-                    bool found = false;
-                    for (int i = 0; i < myList.Count; i++)
-                    {
-                        if (myList[i] == n)
-                        {
-                            found = true;
-                            Console.WriteLine($"needed number in index {i}");
-                            break;
-                        }
-                    }
-                    if (!found)
-                    { Console.WriteLine("needed number not in list!"); }
-                }
-
-                //========search task==========
-                else if (input == 'c' || input == 'C')
-                {
-                    myList.Clear();
-                    Console.WriteLine("list clear successed");
-                }
-                else if (input != 'q' && input != 'Q')
-                {
-                    Console.WriteLine("Unknown selection, please try again");
-                    Console.WriteLine("   =============menu options========");
-                    Console.WriteLine("       p - print numbers");
-                    Console.WriteLine("       A - Add a number");
-                    Console.WriteLine("       M - Display mean of the numbers ");
-                    Console.WriteLine("       S - Display the smallest number");
-                    Console.WriteLine("       L - Display the largest number");
-                    Console.WriteLine("       F - Find anumber");
-                    Console.WriteLine("       C - Clear the whole list");
-                    Console.WriteLine("       Q - Quit");
-                    Console.WriteLine();
-                    Console.WriteLine();
-                    Console.WriteLine("enter your choice from menu");
-                }
-
-
-
-            } while (input != 'q' && input != 'Q');
-            Console.WriteLine("Goodbye");
-
-
-
+            // Returning books
+            Console.WriteLine("Returning books...");
+            library.ReturnBook("Gatsby");
+            library.ReturnBook("Harry Potter"); // This book is not borrowed
+            Console.WriteLine("\n================");
 
 
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
+
+                   
